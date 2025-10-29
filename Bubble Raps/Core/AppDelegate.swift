14c2fbb1a8da1b3ad373a35776dc0cad7b00e144
Alexander Lester <alexander480@gmail.com>
2025-10-29 03:37:30 -0400

    Adopted UIScene

--- Bubble Raps/Core/AppDelegate.swift
+++ Bubble Raps/Core/AppDelegate.swift
@@ -14,12 +14,10 @@ import FirebaseAnalytics
 import GoogleMobileAds
 import SwiftyStoreKit
 
-@UIApplicationMain
+@main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		
 		// MARK: Initialize Firebase
 		FirebaseApp.configure()
@@ -53,31 +51,43 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		// MARK: Initialize Google AdMobs
 		GADMobileAds.sharedInstance().start(completionHandler: nil)
 		
-        return true
-    }
+		return true
+	}
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
+	// MARK: - UIScene session lifecycle
+	func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+		let config = UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+		config.delegateClass = SceneDelegate.self
+		return config
+	}
 
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
+	func application(_ application: UIApplication,
+					 didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+		// Release any resources that were specific to the discarded scenes, as they will not return.
+	}
 
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
+	func applicationWillResignActive(_ application: UIApplication) {
+		// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+		// Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+	}
 
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
+	func applicationDidEnterBackground(_ application: UIApplication) {
+		// Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+		// If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+	}
 
-    func applicationWillTerminate(_ application: UIApplication) {
+	func applicationWillEnterForeground(_ application: UIApplication) {
+		// Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+	}
+
+	func applicationDidBecomeActive(_ application: UIApplication) {
+		// Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+	}
+
+	func applicationWillTerminate(_ application: UIApplication) {
 		// MARK: Save Data Before Termination
 		self.saveContext()
-    }
+	}
 	
 	// MARK: Setup Persistent Container For Core Data
 	lazy var persistentContainer: NSPersistentContainer = {
---
+++ Bubble Raps/Core/SceneDelegate.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+        // If using a storyboard specified in the Info.plist under the Scene Manifest, the system sets up the window and
+        // rootViewController automatically. Do not create the window manually in that case.
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        // Called when the scene has moved from an inactive state to an active state.
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+        // Called when the scene will move from an active state to an inactive state.
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        // Called as the scene transitions from the background to the foreground.
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        // Called as the scene transitions from the foreground to the background.
+    }
+}
